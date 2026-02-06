4c3e148ad62b3c2abddfcedc8a3686fbe08b0b46
Chris Wininger <chris.wininger@gmail.com>
2026-02-06 15:42:40 -0500

    added another project

--- docker-build-output/_site/caw-just-build-it-2026.sh
+++ docker-build-output/_site/caw-just-build-it-2026.sh
@@ -1,4 +1,5 @@
 set -e
+# uncomment if you want to rebuild the image
 ./docker-build_image_and_build_site.sh
 sudo chown chris:chris -R ./
 ./overwrite_pdf.sh ~/Nextcloud/chris-wininger-resume-2026-feb-websafe.pdf
